27570c89999ff3bd0604fa62296345eebc5a7475
omaiyea <omaiyea@gmail.com>
2019-10-11 15:01:02 -0700

    update cors

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
 const helmet = require('helmet');
 
 const app = express();
@@ -10,19 +9,13 @@ const { NODE_ENV } = require('./config');
 const morganOption = (NODE_ENV === 'production' ? 'tiny' : 'common');
 const bookmarkRouter = require('./bookmarks/bookmark-router');
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use(morgan(morganOption));
 app.use(helmet());
-/*
-app.use(function validateBearerToken(req, res, next) {
-  const authToken = req.get('Authorization')
-
-  if (!authToken || authToken.split(' ')[1] !== process.env.API_KEY) {
-    return res.status(401).json({ error: 'Unauthorized request' })
-  }
-
-  next()
-})*/
 
 app.use('/api', bookmarkRouter);
 
